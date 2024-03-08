948e4749a00ace787933b933ad3f78215689afe8
ilanitb16 <ilanit.berdichevski@gmail.com>
2024-03-08 16:50:25 +0200

    final version

--- FMM/create-matrices.py
+++ FMM/create-matrices.py
@@ -6,5 +6,5 @@ fname = input("file name >> ")
 with open(fname, "wb") as f:
     for j in range(n):
         # 65535
-        barr = [randint(0,3).to_bytes(4, byteorder='little') for i in range(n)]
+        barr = [randint(0,65535).to_bytes(4, byteorder='little') for i in range(n)]
         f.write(b''.join(barr))
--- FMM/main.c
+++ FMM/main.c
@@ -9,19 +9,19 @@ int main() {
     int n;
 
 
-//    printf("First matrix: ");
-//    scanf("%s", f1);
-//    printf("Second matrix: ");
-//    scanf("%s", f2);
-//    printf("Result path: ");
-//    scanf("%s", f3);
-//    printf("Size: ");
-//    scanf("%d", &n);
+    printf("First matrix: ");
+    scanf("%s", f1);
+    printf("Second matrix: ");
+    scanf("%s", f2);
+    printf("Result path: ");
+    scanf("%s", f3);
+    printf("Size: ");
+    scanf("%d", &n);
 
 
-    // double time = measure_time(f1, f2, f3, n);
+     double time = measure_time(f1, f2, f3, n);
    // double time = measure_time("../FMM/mat1.txt", "../FMM/mat2.txt", "../FMM/result.txt", 3);
-    double time = measure_time("../FMM/mat1000-1.txt", "../FMM/mat1000-2.txt", "../FMM/result.txt", 1000);
+   // double time = measure_time("../FMM/mat1000-1.txt", "../FMM/mat1000-2.txt", "../FMM/result.txt", 1000);
 
     printf("Total time: %f ms\n", time);
 }
