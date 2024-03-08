80b4581fd4a735eb1fdfbbaf0d294d51bb3e75c2
ilanitb16 <ilanit.berdichevski@gmail.com>
2024-03-08 16:49:15 +0200

    improved code by accessing more

--- FMM/fmm.c
+++ FMM/fmm.c
@@ -8,16 +8,32 @@
 
 void fmm(int n, int* m1, int* m2, int* result) {
 
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < n; j++) {
+//            int sum = 0;
+//            for (int k = 0; k < n; k++) {
+//                // open inner loop:  improve spatial locality by accessing contiguous memory locations within the loop.
+//                sum += m1[i * n + k] * m2[k * n + j];
+//            }
+//            result[i * n + j] = sum;
+//        }
+//    }
+
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            int sum = 0;
-            for (int k = 0; k < n; k++) {
-                // Unroll the inner loop for better performance
-                sum += m1[i * n + k] * m2[k * n + j];
+        for (int k = 0; k < n; k++) {
+            for (int j = 0; j < n; j++) {
+                result[i * n + j] += m1[i * n + k] * m2[k * n + j];
             }
-            result[i * n + j] = sum;
         }
     }
+
+    // result[i * n + j]: element at row i and column j in the result matrix.
+    // m1[i * n + k]: element at row i and column k
+    // m2[k * n + j]: element at row k and column j
+
+    // improve spatial locality by accessing contiguous memory locations within the loop.
+    // Sequential access patterns are more cache-friendly
+
 
 //    for (int i = 0; i < n; i++) {
 //        for (int j = 0; j < n; j++) {
